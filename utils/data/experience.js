286f0498e2816735c38dd0966b7447e415a9e229
Aditya-Karmalkar <126249135+Aditya-Karmalkar@users.noreply.github.com>
2025-01-08 22:49:06 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: '',
+    title: "Intern",
     company: "Soluxction Space LLP.",
     duration: "(2025 - Present)"
   },
   {
     id: 2,
-    title: "",
+    title: "Intern",
     company: "BLU DOLLPPHN",
     duration: "(2023 - Present)"
   },
   {
     id: 3,
     title: "Self Employed",
-    company: "Code and build something in everyday.",
+    company: "Code and build something in every day.",
     duration: "(2022 - Present)"
   }
 ]
